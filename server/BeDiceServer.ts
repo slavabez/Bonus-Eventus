bf109cc4d1079a547252e36527c3b0895e9a0a3b
Slava Bezgachev <bezgachev@gmail.com>
2018-11-01 13:52:18 +0600

    Adding basic tests, first one works

--- server/BeDiceServer.ts
+++ server/BeDiceServer.ts
@@ -2,11 +2,14 @@ import { Server, createServer } from "http";
 //const express = require("express");
 import * as express from "express";
 import * as socketIO from "socket.io";
+import { AddressInfo } from "net";
 
 class BeDiceServer {
   private readonly app: express.Application;
   private readonly server: Server;
   private readonly io: SocketIO.Server;
+
+  public address?: string | AddressInfo;
 
   constructor() {
     this.app = express();
@@ -14,7 +17,7 @@ class BeDiceServer {
     this.io = socketIO(this.server);
   }
 
-  listen(port: number): void {
+  listen(port?: number): void {
     this.server.listen(port, () => {
       console.log(`Be-Dice Server listening on port ${port}`);
     });
@@ -29,9 +32,28 @@ class BeDiceServer {
     });
   }
 
-  handleNewSocketConnected(socket: SocketIO.Socket){
-    // Register all events here
+  getAddress() {
+    if (
+      this.address &&
+      typeof this.address !== "string" &&
+      this.address.address
+    )
+      return this.address.address;
+  }
+  getPort() {
+    if (this.address && typeof this.address !== "string" && this.address.port)
+      return this.address.port;
+  }
 
+  close(): void {
+    this.server.close();
+  }
+
+  handleNewSocketConnected(socket: SocketIO.Socket) {
+    // Register all events here
+    socket.on("ping", () => {
+      socket.emit("pong", () => ({ message: "You bet!" }));
+    });
   }
 }
 
--- server/__tests__/BeDiceServer.test.ts
+++ server/__tests__/BeDiceServer.test.ts
@@ -1,7 +1,50 @@
 import "jest";
+import BeDiceServer from "../BeDiceServer";
+import * as ioClient from "socket.io-client";
 
 describe("Basic connectivity tests", () => {
-  test("Dummy", () => {
-    expect(1).toBe(1);
+  let server: BeDiceServer;
+  let clientSocket: SocketIOClient.Socket;
+
+  beforeAll(() => {
+    server = new BeDiceServer();
+    server.listen();
+  });
+
+  afterAll(() => {
+    server.close();
+  });
+
+  beforeEach(done => {
+    clientSocket = ioClient.connect(
+      `http://[${server.getAddress()}]:${server.getPort()}`,
+      {
+        reconnectionDelay: 0,
+        forceNew: true,
+        transports: ["websocket"]
+      }
+    );
+
+    clientSocket.on("connect", () => {
+      done();
+    });
+  });
+
+  afterEach(done => {
+    if (clientSocket.connected) clientSocket.disconnect();
+    done();
+  });
+
+  test("Can emit a ping and receive a pong in response", done => {
+    clientSocket = ioClient.connect(
+      `http://[${server.getAddress()}]:${server.getPort()}`
+    );
+
+    clientSocket.on("connect", () => {
+      clientSocket.emit("ping");
+    });
+    clientSocket.on("pong", () => {
+      done();
+    });
   });
 });
