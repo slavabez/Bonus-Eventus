ffdf4abb4919e4060f92276e05dfe79129a8a74f
Slava Bezgachev <bezgachev@gmail.com>
2018-11-02 20:10:20 +0600

    Adding NPM commands and more express config and middleware

--- server/BeDiceServer.ts
+++ server/BeDiceServer.ts
@@ -1,8 +1,12 @@
 import { Server, createServer } from "http";
-//const express = require("express");
 import * as express from "express";
 import * as socketIO from "socket.io";
 import { AddressInfo } from "net";
+import * as helmet from "helmet";
+import * as path from "path";
+
+// Express middleware
+import errorMiddleware from "./handlers/errorMiddleware";
 
 // Event handlers
 import { handlePing } from "./handlers/connection";
@@ -18,6 +22,7 @@ class BeDiceServer {
     this.app = express();
     this.server = createServer(this.app);
     this.io = socketIO(this.server);
+    this.setupExpress();
   }
 
   listen(port?: number): void {
@@ -35,7 +40,21 @@ class BeDiceServer {
     });
   }
 
-  getAddress() {
+  setupExpress() {
+    // JSON parser and Helmet for basic security settings
+    this.app.use(helmet()).use(express.json());
+    // Serve static files from React's /build/ folder
+    this.app.use(express.static("client/build"));
+    this.app.get("*", (req: express.Request, res: express.Response) => {
+      // If the route wasn't matched with static, route to index.js
+      res.sendFile(path.join(__dirname + "../client/build/index.html"))
+    });
+
+    // Add basic error handling
+    this.app.use(errorMiddleware);
+  }
+
+  public getAddress() {
     if (
       this.address &&
       typeof this.address !== "string" &&
@@ -44,7 +63,7 @@ class BeDiceServer {
       return this.address.address;
   }
 
-  getPort() {
+  public getPort() {
     if (this.address && typeof this.address !== "string" && this.address.port)
       return this.address.port;
   }
@@ -58,7 +77,6 @@ class BeDiceServer {
     // Register all events here
     socket.on("server.ping", handlePing(socket));
   }
-
 }
 
 export default BeDiceServer;
