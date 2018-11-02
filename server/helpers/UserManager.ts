6c618b49c562efc50e8e1921c6f099e77a0ef36c
Slava Bezgachev <bezgachev@gmail.com>
2018-11-02 23:53:22 +0600

    Adding helpers, fakers and tests
    Signed-off-by: Slava Bezgachev <bezgachev@gmail.com>

--- server/helpers/UserManager.ts
+++ server/helpers/UserManager.ts
@@ -1,9 +1,11 @@
+import * as uniqid from "uniqid";
+
 export interface UserProps {
   avatar: string;
   name: string;
   color: string;
-  id: string;
-  socketId?: string;
+  id?: string;
+  socketId: string;
 }
 
 export class User {
@@ -11,30 +13,65 @@ export class User {
   public name: string;
   public color: string;
   public id: string;
-  public socketId?: string;
+  public socketId: string;
 
   constructor(props: UserProps) {
     // Wanted to use Object.assign, but typescript doesn't like it
-    this.id = props.id;
-    this.name = props.name;
-    this.avatar = props.avatar;
-    this.color = props.color;
-    this.socketId = props.socketId;
+    // If overly long values are passed, cut to reasonable size
+    if (props.id) {
+      this.id = props.id.length > 25 ? props.id.substr(0, 15) : props.id;
+    } else {
+      this.id = uniqid();
+    }
+    this.name = props.name.length > 15 ? props.name.substr(0, 15) : props.name;
+    this.avatar =
+      props.avatar.length > 255 ? props.avatar.substr(0, 255) : props.avatar;
+    this.color =
+      props.color.length > 7 ? props.color.substr(0, 7) : props.color;
+    this.socketId =
+      props.socketId.length > 25
+        ? props.socketId.substr(0, 15)
+        : props.socketId;
   }
 }
 
 export default class UserManager {
-  public allUsers?: Map<string, User>;
+  public allUsers!: Map<string, User>;
 
-  constructor(){
+  constructor() {
     this.allUsers = new Map();
   }
 
-  registerNewUser(props: UserProps){
+  registerNewUser(props: UserProps): boolean | User {
     // Make sure no user with such ID is already registered
-    if (!this.allUsers.has(props.id)) {
-      return;
+    if (!props.id) {
+      return false;
+    }
+    if (this.allUsers.has(props.id)) {
+      return false;
+    }
+    const u = new User(props);
+    this.allUsers.set(props.id, u);
+    return u;
+  }
+
+  updateSocketId(userId: string, newId: string) {
+    if (this.allUsers.has(userId)) {
+      // Neat, a non-null assertion operator (!)
+      this.allUsers.get(userId)!.socketId = newId;
     }
   }
 
+  findUserBySocketId(socketId: string): User | undefined {
+    // Some neat es6 array methods
+    return Array.from(this.allUsers.values()).find(
+      u => u.socketId === socketId
+    );
+  }
+
+  deleteUser(id: string) {
+    if (this.allUsers.has(id)) {
+      this.allUsers.delete(id);
+    }
+  }
 }
