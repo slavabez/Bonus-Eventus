184778eed8a8ef436ce05fbf9bcae892b6060c75
Slava Bezgachev <bezgachev@gmail.com>
2018-11-14 17:23:44 +0600

    Adding header framework
    Signed-off-by: Slava Bezgachev <bezgachev@gmail.com>

---
+++ client/src/components/Header/ProfileControls.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+
+`;
+
+class ProfileControls extends Component {
+  render() {
+    return (
+      <Wrapper>
+        <button>Exit room</button>
+        <button>Profile</button>
+      </Wrapper>
+    );
+  }
+}
+
+export default ProfileControls;
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,11 +1,37 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import styled from "styled-components";
+import ProfileControls from "./ProfileControls";
+
+import logo from "../../assets/logo.png";
+
+const Spacer = styled.div`
+  width: 250px;
+  display: none;
+
+  @media (min-width: 768px) {
+    display: block;
+  }
+`;
+
+const Wrapper = styled.header`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  img {
+    max-height: 100px;
+  }
+`;
 
 class Header extends Component {
   render() {
     return (
-      <div>
-        I'm a header!
-      </div>
+      <Wrapper>
+        <Spacer />
+        <img src={logo} alt="BE Dice Logo (Bonus Eventus)" />
+        <ProfileControls />
+      </Wrapper>
     );
   }
 }
